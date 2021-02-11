97aae816d4922d6295102e00b182711ba0c05365
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-02-11 13:02:16 +0000

    printing out statements

--- helpers/copyJob.groovy
+++ helpers/copyJob.groovy
@@ -2,8 +2,11 @@ import hudson.model.*
 
 for (item in Hudson.instance.items) {
     jobToBeCopied = 'cucumber_browser_tests.groovy'
+    println jobToBeCopied
     duplicateJobName = '${jobsToBeCopied}-duplicate'
+    println jobToBeCopied
     if (item.name == jobToBeCopied.replaceAll('.groovy', '')) {
+        println "COPYHAPPENING"
         Hudson.instance.copy(item, duplicateJobName).save()
     }
 }
