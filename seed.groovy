2a78cadbaf194998f98b907446f202d73e9b6875
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-02-11 10:33:36 +0000

    trying class path

--- seed.groovy
+++ seed.groovy
@@ -3,6 +3,7 @@ pipeline {
     stages {
         stage('createjobs') {
             steps {
+                shell ('set CLASSPATH=jobs/common')
                 script {
                     jobDsl targets: 'jobs/*.groovy', removedJobAction: 'DELETE', removedViewAction: 'DELETE'
                 }
