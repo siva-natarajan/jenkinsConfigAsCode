b44a197b908709e70651a018d4a6fd7fc3ddb7c8
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-19 12:04:10 +0000

    adding slack notification

--- jobs/fileArtifact.groovy
+++ jobs/fileArtifact.groovy
@@ -15,7 +15,14 @@ job('fileArtifact') {
         shell('''
         echo DONE EXECUTION
         ''')
+        def command = String.format(
+            'curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d \'{"resultDescription": "%s", "channel": "@sivakumar ", "buildNumber": "%s"}\' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io',
+            '23 passed, 24 failed',
+            '123'
+        )
+        shell(command)
     }
+
     publishers {
         archiveArtifacts {
             pattern('**/newFile.json')
@@ -23,4 +30,7 @@ job('fileArtifact') {
             allowEmpty(true)
         }
     }
+
+    def command = String.format('curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d \'{"testStage": "%s", "link": "%s", "ts": "%s", "channel": "#backend-notifications", "requestor": "%s"}\' https://f3247171-9315-4217-93fe-212f8b037559.trayapp.io', testStage, testResultsLink, slackThreadId, env.DEPLOY_REQUESTOR)
+    sh(command)
 }
