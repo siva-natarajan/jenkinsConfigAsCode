2a1ee674e169e08871dbfbe7ac8ee19e70b9624a
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-19 07:02:05 +0000

    corrected job

--- jobs/fileArtifact.groovy
+++ jobs/fileArtifact.groovy
@@ -1,12 +1,20 @@
 job('fileArtifact') {
-    steps {
+    scm {
         git {
             remote {
                 url('https://github.com/siva-natarajan/sampleJsonFileGenerator.git')
             }
             branch('*/main')
         }
+    }
+    steps {
+        shell('''
+        echo STARTED EXECUTION
+        ''')
         shell('sh ./src/execute.sh')
+        shell('''
+        echo DONE EXECUTION
+        ''')
     }
     publishers {
         archiveArtifacts {
