b66dfe5995a8d249566bf7fd6acba9e8369c2aa3
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-19 06:36:16 +0000

    adding multiple files

--- jobs/new.groovy
+++ jobs/new.groovy
@@ -6,13 +6,16 @@ job('new') {
         shell('''
         echo 'This is a test' > data.txt
         ''')
+        shell('''
+        echo 'This is a test' > dataTest.txt
+        ''')
     }
     publishers {
         allure(['allure-results-combined']) {
         // Bracket body is needed!
         }
         archiveArtifacts {
-            pattern('**/data.txt')
+            pattern('**/data.txt', 'allure-report.zip', '**/dataTest.txt')
             allowEmpty(true)
         }
     }
