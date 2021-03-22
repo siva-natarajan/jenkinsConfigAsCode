a2448fc292439de8ca3cbe0e54af008482660c25
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-22 06:36:59 +0000

    moving build step

--- jobs/fileArtifact.groovy
+++ jobs/fileArtifact.groovy
@@ -24,7 +24,7 @@ job('fileArtifact') {
     steps {
         def command = String.format(
             'curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d \'{"channel": "@sivakumar", "buildNumber": "%s"}\' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io',
-            env.BUILD_NUMBER,
+            BUILD_NUMBER,
         )
         shell(command)
     }
