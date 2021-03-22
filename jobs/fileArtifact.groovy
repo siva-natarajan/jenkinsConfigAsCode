99526727728a4591eba52c936d4af37d2cfa4901
Siva <71319212+siva-natarajan@users.noreply.github.com>
2021-03-22 06:35:57 +0000

    moving build step

--- jobs/fileArtifact.groovy
+++ jobs/fileArtifact.groovy
@@ -12,22 +12,20 @@ job('fileArtifact') {
         echo STARTED EXECUTION
         ''')
         shell('sh src/execute.sh')
-        shell('''
-        echo DONE EXECUTION
-        ''')
-        def command = String.format(
-            'curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d \'{"resultDescription": "%s", "channel": "@sivakumar ", "buildNumber": "%s"}\' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io',
-            '23 passed, 24 failed',
-            '123'
-        )
-        shell(command)
     }
 
     publishers {
         archiveArtifacts {
             pattern('**/newFile.json')
-            pattern('**/newFile.txt')
             allowEmpty(true)
         }
+    }
+
+    steps {
+        def command = String.format(
+            'curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d \'{"channel": "@sivakumar", "buildNumber": "%s"}\' https://731e8c31-f578-4b67-b5cb-e2f17dee6e50.trayapp.io',
+            env.BUILD_NUMBER,
+        )
+        shell(command)
     }
 }
